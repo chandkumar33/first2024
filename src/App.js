0862f59da39a5e095736ed281d577391a60f4f6d
Chand Kumar Koli <rkkohli57@gmail.com>
2024-10-19 10:21:56 +0530

    Home page design changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
 import './App.css';
-import Header from './components/Header/Header';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
-    <Header/>
+     <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    
+    extend: {
+      colors: {
+        'primary': '#35ac75',
+      },
+    },
   },
   plugins: [],
 }
